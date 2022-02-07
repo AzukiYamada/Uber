a09b5f18b00e2314cca13f0fd497feace5a4c747
Azuki Yamada <yamada.azuki@aoaoi.work>
2022-02-07 14:19:25 +0900

    fix/210207/-2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import tw from "tailwind-styled-components"
 
 export default function Home() {
   return (
+<<<<<<< Updated upstream
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -65,5 +67,24 @@ export default function Home() {
         </a>
       </footer>
     </div>
+=======
+    <Wrapper>
+      <Map>Map</Map>
+      <ActionItems>start</ActionItems>
+    </Wrapper>
+>>>>>>> Stashed changes
   )
 }
+
+const Wrapper = tw.div`
+  flex
+  flex-col
+  bg-red-300
+  h-screen 
+`
+const Map = tw.div`
+  flex-1
+`
+const ActionItems = tw.div`
+  flex-1
+`
