5c183d1d6cf16503816abeba84dacc531ef5d166
Azuki Yamada <yamada.azuki@aoaoi.work>
2022-02-07 14:41:34 +0900

    install mapbox

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+<<<<<<< Updated upstream
 import styles from '../styles/Home.module.css'
+import tw from "tailwind-styled-components"
+import mapboxgl from '!mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXp1a2l5YW1hZGEiLCJhIjoiY2t6Yzh4cm9rMmp4NjJucHZuZjl6NGo2cCJ9.JLbhcNhvlfrBN2hQ3UHqbA';
 
 export default function Home() {
+
+  useEffect (() => {
+    console.log('hello')
+  },[])
+
   return (
     <div className={styles.container}>
       <div className={styles.map}>Map</div>
