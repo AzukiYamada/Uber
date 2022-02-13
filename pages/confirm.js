ef28e8c464ecf4066efcb6d17d6a530224fe95c1
Azuki Yamada <yamada.azuki@aoaoi.work>
2022-02-13 15:33:59 +0900

    Create confirm.js

---
+++ pages/confirm.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import tw from "tailwind-styled-components"
+import Map from './components/Map';
+
+const confirm = () => {
+    return (
+        <Wrapper>
+            <Map />
+
+            <RideContainer>
+
+            </RideContainer>
+        </Wrapper>
+    )
+};
+
+export default confirm;
+
+const Wrapper = tw.div`
+    flex
+    flex-col
+    h-screen
+`
+
+const RideContainer = tw.div`
+    flex-1
+`
